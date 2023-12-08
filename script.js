3c71cb202342dc22e657a8e46e049d273e531f06
Lizzon <lvdv4j@gmail.com>
2023-12-08 15:45:51 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function bttnAlert() {
+     // Fetch the value of the email input field
+     let emailValue = document.getElementById('email').value;
+
+     // Display the email address in the alert
+     alert(`If this company was real, your email (${emailValue}) would have been submitted successfully :)`);
+}
+
+function searchAlert(){
+    alert("This is a magnifying glass... Hope you weren't expecting something else XD");
+}
